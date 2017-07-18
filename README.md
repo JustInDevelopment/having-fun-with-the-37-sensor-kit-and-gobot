2c58d83030c515d8ae01e264878f8af159f6665d
Justin Zemlyansky <JustInDevelopment@users.noreply.github.com>
2017-07-17 19:44:52 -0700

    just some fun
    rando fun stuff

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # having fun with the 37 sensor kit and gobot
+## nothing speacial here 
+### code is in examples
+
+This uses firmata and gobot!!!
+
---
+++ examples/joystick.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/drivers/gpio"
+	"gobot.io/x/gobot/platforms/firmata"
+)
+
+func main() {
+	firmataAdaptor := firmata.NewAdaptor("COM113")
+	ledGreen := gpio.NewLedDriver(firmataAdaptor, "6")
+	ledBlue := gpio.NewLedDriver(firmataAdaptor, "5")
+	ledRed := gpio.NewLedDriver(firmataAdaptor, "3")
+	ledCount := 0
+	rgb := ledBlue
+
+	work := func() {
+
+		for light := 0; light <= 255; light++ {
+
+			for light := 255; light >= 0; light-- {
+
+				light := byte(light)
+				rgb.Brightness(light)
+
+			}
+			light := byte(light)
+			rgb.Brightness(light)
+			if ledCount == 0 {
+				rgb = ledGreen
+				ledBlue.Off()
+				ledRed.Off()
+			}
+
+			if ledCount == 1 {
+				rgb = ledBlue
+				ledGreen.Off()
+				ledRed.Off()
+			}
+
+			if ledCount == 2 {
+				rgb = ledRed
+				ledBlue.Off()
+				ledGreen.Off()
+			}
+			time.Sleep(time.Millisecond * 30)
+			ledCount++
+			if ledCount == 3 {
+				ledCount = 0
+			}
+
+		}
+
+	}
+
+	robot := gobot.NewRobot("sensorBot",
+		[]gobot.Connection{firmataAdaptor},
+		[]gobot.Device{ledGreen, ledBlue, ledRed},
+		work,
+	)
+
+	robot.Start()
+
+}
